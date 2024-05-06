6bd5c29428e27e5d093c97b0467c1ef2e14a1383
Eiji <eiji@OMEN>
2024-05-06 14:27:42 +0900

    0506

--- site.config.js
+++ site.config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "学生",
     bio: "",
-    email: "asuka.saito.n46(at)gmail.com",
-    linkedin: "",
+    //email: "asuka.saito.n46(at)gmail.com",
+    linkedin: "rongzhi-li-8447811ab",
     github: "des737",
     instagram: "",
   },
